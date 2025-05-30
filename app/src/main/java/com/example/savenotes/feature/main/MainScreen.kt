5d5ec7e836e1a4582efb4494d52318486109a9f3
daniospina <ingedanielospina9@gmail.com>
2025-05-30 12:19:15 -0500

    le puse algo de padding al column principal, y lo centré pa que quede mas pleno. Además

--- app/src/main/java/com/example/savenotes/feature/main/MainScreen.kt
+++ app/src/main/java/com/example/savenotes/feature/main/MainScreen.kt
@@ -2,9 +2,12 @@ package com.example.savenotes.feature.main
 
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -12,11 +15,13 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlin.collections.get
 
 @Composable
 fun MainScreen(
@@ -39,7 +44,11 @@ fun MainScreen(
 
     Scaffold { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             TextField(
                 value = state.text,
@@ -55,7 +64,7 @@ fun MainScreen(
                 )
             }
 
-            LazyColumn(
+            /*LazyColumn(
                 modifier = Modifier
                     .fillMaxWidth()
                     .weight(1f)
@@ -67,6 +76,23 @@ fun MainScreen(
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(16.dp),
+                        text = notes[it].text
+                    )
+                }
+            }
+
+             */
+
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(.dp),
+            ) {
+                items(
+                    count = notes.size
+                ) {
+                    Text(
+                        modifier = Modifier
+                            .padding(12.dp)
+                            .fillMaxWidth(),
                         text = notes[it].text
                     )
                 }
